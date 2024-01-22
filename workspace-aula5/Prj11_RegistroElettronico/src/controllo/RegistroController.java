d3d027645da6484059669eafa2fae732f1e9fddb
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2024-01-22 12:57:02 +0100

    aggiornamento da I&L

---
+++ workspace-aula5/Prj11_RegistroElettronico/src/controllo/RegistroController.java
@@ -0,0 +1,77 @@
+package controllo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import modello.Registro;
+import modello.Studente;
+
+public class RegistroController {
+
+	private Registro registro;
+	private ArrayList<Studente> presenti;
+	private ArrayList<Studente> assenti;
+
+	public RegistroController(Registro registro) {
+		this.registro = registro;
+		this.presenti = new ArrayList<Studente>();
+		this.assenti = new ArrayList<Studente>();
+	}
+
+	public void caricaStudentiDaFile() {
+		
+		File fileDiTesto = new File("studenti.txt");
+		
+		try {
+			Scanner input = new Scanner(fileDiTesto);
+			
+			while(input.hasNextLine()) {
+				String line = input.nextLine();
+				String[] parole = line.split(",");
+				String nomeStudente = parole[0];
+				String cognomeStudente = parole[1];
+				Studente s = new Studente(nomeStudente, cognomeStudente);
+				this.registro.aggiungiStudente(s);
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+	
+	public void faiAppello() {
+		Scanner input = new Scanner(System.in);
+		
+		for(Studente s : this.registro.getAllStudents()) {
+			System.out.println("E' presente lo studente");
+			System.out.println(s);
+			String presente = input.nextLine();
+			
+			if (presente.equals("s")) {
+				presenti.add(s);
+			} else {
+				assenti.add(s);
+			}
+		}
+	}
+	
+	public void stampaPresenti() {
+		for (Studente studente : presenti) {
+			System.out.println(studente);
+		}
+		System.out.println("Sono presenti " + presenti.size() + " studenti");
+	}
+	
+	public void stampaAssenti() {
+		for (Studente studente : assenti) {
+			System.out.println(studente);
+		}
+		System.out.println("Sono assenti " + assenti.size() + " studenti");
+	}
+	
+}
--- workspace-aula5/Prj11_RegistroElettronico/src/vista/RegistroDemo.java
+++ workspace-aula5/Prj11_RegistroElettronico/src/vista/RegistroDemo.java
@@ -1,5 +1,6 @@
 package vista;
 
+import controllo.RegistroController;
 import modello.Registro;
 import modello.Studente;
 
@@ -8,18 +9,23 @@ public class RegistroDemo {
 	public static void main(String[] args) {
 
 		Registro tss2024 = new Registro("Corso TSS 2024");
+		RegistroController ctrl = new RegistroController(tss2024); 
+		ctrl.caricaStudentiDaFile();
+		ctrl.faiAppello();
+		ctrl.stampaPresenti();
+		ctrl.stampaAssenti();
 		
 		//Studente s1 = new Studente("giulia", "artuso");
 		//Studente s2 = new Studente("leonardo", "castaldo");
 
-		tss2024.aggiungiStudente(new Studente("giulia", "artuso"));
-		tss2024.aggiungiStudente(new Studente("leonardo", "castaldo"));
+//		tss2024.aggiungiStudente(new Studente("giulia", "artuso"));
+//		tss2024.aggiungiStudente(new Studente("leonardo", "castaldo"));
 		
-		tss2024.getStudenteById(0);
-		
-		for(Studente s : tss2024.getAllStudents()) {
-			System.out.println(s);
-		}
+//		tss2024.getStudenteById(0);
+//		
+//		for(Studente s : tss2024.getAllStudents()) {
+//			System.out.println(s);
+//		}
 
 		
 		
